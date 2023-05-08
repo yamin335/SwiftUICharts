8a6979d14c242b389882bdfda800358cee2329e4
Yamin <myamin335@gmail.com>
2023-05-08 19:43:53 +0200

    Initial Commit

---
+++ SwiftUICharts/SwiftUIChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIChartsApp.swift
+//  SwiftUICharts
+//
+//  Created by Md. Yamin on 08.05.23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
